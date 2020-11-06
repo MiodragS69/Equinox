26d91730afd1459cdd461196c7263379f9026a45
miodrag.sabljic <sabljic.miodrag@gmail.com>
2020-11-06 23:05:56 +0100

    Add Question fluent API configuration

--- src/Equinox.Questions.Entities/FluentApiMappers/QuestionEntityConfiguration.cs
+++ src/Equinox.Questions.Entities/FluentApiMappers/QuestionEntityConfiguration.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Equinox.Questions.Entities.Model;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Equinox.Questions.Entities.Enums;
 
 namespace Equinox.Questions.Entities.FluentApiMappers
 {
@@ -11,7 +12,18 @@ namespace Equinox.Questions.Entities.FluentApiMappers
     {
         public void Configure(EntityTypeBuilder<Question> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(q => q.Id);
+            builder.Property(q => q.Text)
+                .IsRequired()
+                .HasMaxLength(500);
+            builder.Property(q => q.IsDeleted)
+                .HasDefaultValue(false);
+            builder.Property(q => q.MultiAnswer)
+                .HasDefaultValue(false);
+            builder.Property(q => q.QuestionGrade)
+                .HasDefaultValue(Grade.NoGrade);
+            builder.Property(q => q.QuestionScope)
+                .HasDefaultValue(Scope.NoScope);
         }
     }
 }
